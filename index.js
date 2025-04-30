b47e4c3a2323eddf729c4fcee090f6c6846217bc
ANTONY VALENTIM GARCIA DOS SANTOS <antony.equisul@gmail.com>
2025-04-30 09:12:57 -0300

    [UPDATE] Simplifying readFile

--- index.js
+++ index.js
@@ -1,66 +1,96 @@
 const express = require('express');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = 8000;
+const novoId = uuidv4();
 app.use(express.json());
 
-const bancoDeDados =[]
+function lerBancoDeDados(callback) {
+    fs.readFile('bancoDeDados.json', 'utf-8', (err, data) => {
+        if (err) return callback(err);
+        try {
+            const json = JSON.parse(data);
+            callback(null, json);
+        } catch (parseErr) {
+            callback(parseErr);
+        }
+    });
+}
 
 app.get('/aulas', (req, res) => {
-    fs.readFile('bancoDeDados.json', 'utf-8', (err, data) => {
-        if(err){
-            res.status(500).json({error: 'Erro ao ler o banco de dados'});
+    lerBancoDeDados((err, aulas) => {
+        if (err) {
+            return res.status(500).json({ error: 'Erro ao ler o banco de dados' });
         }
-        res.status(200).json(JSON.parse(data));
+        res.status(200).json(aulas);
     });
 });
 
 app.get('/aulas/:id', (req, res) => {
     const id = req.params.id;
-    fs.readFile('bancoDeDados.json', 'utf-8', (err, data) => {
+    lerBancoDeDados((err, aulas) => {
         if(err){
-            res.status(500).json({error: 'Erro ao ler o banco de dados'});
+            return res.status(500).json({error: 'Erro ao ler o banco de dados'});
         }
-        const aulas = JSON.parse(data);
         const aula = aulas.find(aula => aula.id == id);
         if(aula){
-            res.status(200).json(aula);
+            return res.status(200).json(aula);
         }
         res.status(404).json({error: 'Usuário não encontrado'});
     });
 })
 
 app.post('/aulas', (req, res) => {
-    fs.readFile('bancoDeDados.json', 'utf-8', (err, data) => {
+    lerBancoDeDados((err, aulas) => {
         if(err){
-            res.status(500).json({error: 'Erro ao ler o banco de dados'});
+            return res.status(500).json({error: 'Erro ao ler o banco de dados'});
         }
-        const aulas = JSON.parse(data);
-        dados['id'] = aulas.length + 1;
+        const dados = req.body;
+        dados['id'] = uuidv4();
         aulas.push(dados);
-        fs.writeFile('bancoDeDados.json', aulas, (err) => {
+        fs.writeFile('bancoDeDados.json', JSON.stringify(aulas), (err) => {
             if(err){
-                res.status(500).json({error: 'Erro ao salvar o banco de dados'});
+                return res.status(500).json({error: 'Erro ao salvar o banco de dados'});
             }
+            res.status(200).json(dados);
         });
-        res.status(200).json(JSON.parse(data));
     });
 });
 
 app.put('/aulas/:id', (req, res) => {
     const id = req.params.id;
-    const usuario = bancoDeDados.find(aula => aula.id == id);
-    if(!usuario){
-        res.status(404).json({error: 'Usuário não encontrado'});
-    }
+    lerBancoDeDados((err, aulas) => {
+        if(err){
+            return res.status(500).json({error: 'Erro ao ler o banco de dados'});
+        }
+        const aulaIndex = aulas.findIndex(aula => aula.id == id);
+        if(aulaIndex !== -1){
+            const dados = req.body;
+            for(const key in dados){
+                aulas[aulaIndex][key] = dados[key];
+            }
+            fs.writeFile('bancoDeDados.json', JSON.stringify(aulas), (err) => {
+                if(err){
+                    return res.status(500).json({error: 'Erro ao salvar o banco de dados'});
+                }
+            });
+            res.status(200).json(aulas[aulaIndex]);
+        } else {
+            return res.status(404).json({error: 'Aula não encontrado'});
+        }
+    })
 })
 
 app.delete('/aulas/:id', (req, res) => {
-    const id = req.params.id;
+    lerBancoDeDados((err, aulas) => {
+        if(err){
+            return res.status(500).json({error: 'Erro ao ler o banco de dados'});
+        }});
     const usuario = bancoDeDados.findIndex(aula => aula.id == id);
     if(usuario === -1){
-        res.status(404).json({error: 'Usuário não encontrado'});
+        return res.status(404).json({error: 'Usuário não encontrado'});
     }
     bancoDeDados.splice(usuario, 1);
     res.status(204).send();
